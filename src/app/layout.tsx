f7239f1390773724bd4230c808eced0ef663c7a2
Guggillanithin <217r1a6782@cmrtc.ac.in>
2026-05-29 14:24:48 +0530

    added Google analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -145,6 +145,25 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
         />
+        
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-JRETHFVWB8"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JRETHFVWB8');
+            `,
+          }}
+        />
+
         <AssessmentProvider>
           {children}
         </AssessmentProvider>
